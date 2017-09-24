a66e0ff0e141420103625f91e1388828035aaff7
liukai <liu-kevin@foxmail.com>
2017-09-24 20:07:29 +0800

    增加回调接口

---
+++ src/main/java/com/liukevin/util/inter/callback/CallBack.java
@@ -0,0 +1,16 @@
+package com.liukevin.util.inter.callback;
+
+import java.util.Map;
+
+/**
+ * 回调方法
+ * Created by liukai on 2017/9/24.
+ */
+public interface CallBack {
+
+    /**
+     * 入参为map的回调方法
+     * @param resultMap
+     */
+    void callBack(Map<String, String> resultMap);
+}
